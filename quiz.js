08ffd99048893a623ddb0b0dee84caa27c8e086f
Tunahan Öztürk <tnhn17@windowslive.com>
2023-06-06 01:28:27 +0300

    aaa

---
+++ quiz.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "Which is the most popular football club in the world?",
+        answers: [
+            { text: "Milan", correct: false },
+            { text: "Barcelona", correct: false },
+            { text: "Manchester City", correct: false },
+            { text: "Real Madrid", correct: true},
+        ]
+    },
+    {
+        question: "Which city is crowder than other cities?",
+        answers: [
+            { text: "İstanbul", correct: false },
+            { text: "New York", correct: true },
+            { text: "Tokyo", correct: false },
+            { text: "Berlin", correct: false},
+        ]
+    },
+    {
+        question: "Who is the richest human in the world?",
+        answers: [
+            { text: "Bernard Arnault", correct: true },
+            { text: "Elon Musk", correct: false },
+            { text: "Ali Koç", correct: false },
+            { text: "Donald Trump", correct: false},
+        ]
+    },
+    {
+        question: "Who is the most scored in NBA?",
+        answers: [
+            { text: "Kareem Abdul-Jabbar", correct: false },
+            { text: "Michael Jordan", correct: false },
+            { text: "Kobe Bryant", correct: false },
+            { text: "Lebron James", correct: true},
+        ]
+    },
+
+]
