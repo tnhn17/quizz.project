a63c041b9c0cdf0ff485c4689260adb497b29767
Tunahan Öztürk <tnhn17@windowslive.com>
2023-06-06 18:54:17 +0300

    Yeni düzenlemeler ve Soruları getirme eklentileri eklendi!

--- quiz.js
+++ quiz.js
@@ -37,3 +37,42 @@ const questions = [ // Soruların içerdiği const yapısı
     },
 
 ]
+
+const questionElement = document.getElementById("question"); //Html sayfasındaki soruyu alacağımız id
+const answerButtons = document.getElementById("answer-buttons"); // Cevapların id'sini alacağımız yer
+const nextButton = document.getElementById("next-btn"); // Geçiş butonunun id'sini aldığımız yer
+
+let currentQuestionIndex = 0; // Index 0'dan başlayacak
+let score = 0; // Başlangıç skoru
+
+function startQuiz() {   // Quiz başlangıç fonksiyonunu oluşturuyoruz
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next"; // innerHTML sayesinde bir elementin içindeki HTML'leri değiştirebilirim.
+    showQuestion();
+
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex]; // Mevcut sorunun indexini alıyor
+    let questionNo = currentQuestionIndex + 1; // Indexe göre alacağı için index 0 olursa soru numarası 1 olur
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question; 
+
+    currentQuestion.answers.forEach(answer => { // Döngü aracılığıyla answer'i alıyor
+        const button = document.createElement("button")
+        button.innerHTML = answer.text; // HTML'e answer'ı ekliyor
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+    })
+}  
+
+
+function resetState() {
+    nextButton.style.display = "none"
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+startQuiz();
